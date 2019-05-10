36a5525ff5ac4777c3b37b51aff66d23d85f0cdd
Griffin <griffincriste@gmail.com>
2019-05-09 20:20:08 -0500

    fixed letter input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,13 @@ var currentword = document.getElementById("currentWord");
 var guessedLetters = document.getElementById("guessedLetters");
 var remainingGuesses = document.getElementById("remainingGuess");
 var wins = document.getElementById("wins");
+var winNum = 0;
+var loseNum = 0;
 var losses = document.getElementById("losses");
 var guessesLeft = 10;
-
-
+var gameStart = false;
+var answer = [];
+var firstTime = true;
 
 directions.textContent = "Press any key to start the game!";
 currentWord.textContent = "Current Word: ";
@@ -19,39 +22,72 @@ remainingGuesses.textContent = "Remaining Guesses: ";
 wins.textContent = "Wins: ";
 losses.textContent = "Losses: ";
 
-//set up the word after user hits any key
-document.onkeyup = function(event) {
 
-    var initialHit = event.key;
-    directions.textContent = "";
-    var word = wordBank[Math.floor(Math.random()*wordBank.length)];
+var word = wordBank[Math.floor(Math.random()*wordBank.length)];
     console.log(word)
+//set up the word after user hits any key
 
+
+
+
+
+
+
+document.onkeyup = function(event) {
+    gameStart = true;
+
+    
+    var yourGuess = event.key
     remainingGuesses.textContent = "Remaining Guesses: " + guessesLeft;
+    wins.textContent = "Wins: " + winNum;
+    losses.textContent = "Losses: " + loseNum;
 
-
-  
-   
     
-   document.onkeyup = function(event){
-    var answer = [];
     for(var i = 0; i < word.length; i++) {
-        answer[i] = "_ ";
-      currentWord.textContent = "Current Word: " + answer;
-     
-    
-        
-   var yourGuess = event.key
-    for (var j = 0; j < word.length; j++){
-        if (word[j] === yourGuess){
-            answer[j] = yourGuess;
-           
+        if (word[i] === yourGuess){
+            answer[i] = yourGuess;
+             
+        }
+        else if(answer[i]!=yourGuess || word[i]!=yourGuess ){
             guessesLeft-=1;
-            
+
         };
-    };
+        if (firstTime) {
+            answer[i] = "_ ";  
+          
+        };
+
+       
+
 
     };
-    };
+
+    firstTime=false;
+    currentWord.textContent = "Current Word: " + answer;
+   
+    win();
+    Loss();
+            
+
+           // guessesLeft-=1;
 };
+    //else if (word[j] != yourGuess){
+           // answer[j] != yourGuess;
+            //guessedLetters = guessedLetters + yourGuess;
+    function wron  
 
+            
+function win(){
+
+    if (answer.indexOf("_") ===-1){
+        winNum++;
+        wins.textContent = "wins"
+    }
+}
+
+function Loss(){
+
+    if (guessesLeft <= 0){
+        loseNum++;
+    }
+};
